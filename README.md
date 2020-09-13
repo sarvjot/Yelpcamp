bde0cbe0a5e438e7d70baa2a3636b4fbee92f16a
Sarvjot Singh <sarvjotsingh422002@gmail.com>
2020-09-13 16:59:04 +0530

    Added Database

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 * this repo is based upon a project from Colt Steele's Web Developer Bootcamp
 * I am really thankful to Colt, I learnt so much
 * Keep doing the good work brother!
+
+# Routes
+   name      url           verb           description
+======================================================
+* INDEX     /dogs           GET     Display a list of all dogs
+* NEW       /dogs/new       GET     Displays a form to make a new dog
+* CREATE    /dogs           POST    Add new dog to DB
+* SHOW      /dogs/:id       GET     Show info about one dog
--- app.js
+++ app.js
@@ -1,40 +1,77 @@
-var express = require("express");
-var app = express();
-var bodyParser = require('body-parser');
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose');
 
-app.use(bodyParser.urlencoded({extended: true}));``
+mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
+// Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-];
+var Campground = mongoose.model("Campground", campgroundSchema);        
+// Campground <==> db.campgrounds
+
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log("Newly Created Campground :");
+//             console.log(campground);
+//         }
+//     }
+// );
+
+
 
 
 app.get("/", (req,res) => {
     res.render("landing");
 })
+// INDEX Route - Display a list of all campgrounds
 app.get("/campgrounds", (req,res) => {
-
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 })
-
+// CREATE Route - Add a new campground "to the data base"
 app.post("/campgrounds", (req,res) => {
-    
     // get data from form and add that to campgrounds page
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    // redirect to the campgrounds page
-    res.redirect("/campgrounds")
+    // Create a new campground and save it to database
+    Campground.create(newCampground, function(err,newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            // redirect to the campgrounds page
+            res.redirect('/campgrounds');
+        }
+    });
 });
-
+//NEW Route - Display a form to create a new campground via CREATE Route    
 app.get("/campgrounds/new", (req,res) => {
     res.render('new');
 });
+
+app.get("/campgrounds/:id", (req,res) => {
+    res.send("This will be the Show page one day!");
+})
+
 app.listen(3000, () => {
     console.log("The YelpCamp Server has started!");
 })
