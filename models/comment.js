b6fe7590ede1d06fec0f0cc5791f2dcf8402b892
Sarvjot Singh <sarvjotsingh422002@gmail.com>
2020-09-16 17:39:21 +0530

    Added Comment Model

---
+++ models/comment.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var commentSchema = mongoose.Schema({
+    text: String,
+    author: String
+})
+var Comment = mongoose.model("Comment", commentSchema);
+module.exports = Comment;
